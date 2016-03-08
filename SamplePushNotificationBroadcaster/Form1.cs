55d8578b76adbaede77c58883780bcfd162d6c95
rohitvipin <rohit.vipinmathews@cesltd.com>
2016-03-08 14:03:05 +0530

    APNS

--- SamplePushNotificationBroadcaster/Form1.cs
+++ SamplePushNotificationBroadcaster/Form1.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using PushSharp.Apple;
 using PushSharp.Google;
 using SamplePushNotificationBroadcaster.Properties;
 
@@ -15,8 +12,10 @@ namespace SamplePushNotificationBroadcaster
 {
     public partial class Form1 : Form
     {
-        private const string SenderId = "927829574554";
-        private const string SenderAuthToken = "AIzaSyBpSNEc5N3wdhzyGGDdZ5pZkOMzYqkJ7lA";
+        private const string GcmSenderId = "927829574554";
+        private const string GcmSenderAuthToken = "AIzaSyBpSNEc5N3wdhzyGGDdZ5pZkOMzYqkJ7lA";
+        private const string ApnsCertificateFile = @"\Resources\PushCertificates.p12";
+        private const string ApnsCertificatePassword = "Welcome123";
 
         public Form1()
         {
@@ -50,35 +49,55 @@ namespace SamplePushNotificationBroadcaster
             {
                 return;
             }
+
             progressBar1.Value = 5;
+            progressBar1.Value += 5;
+
+            var notificationPayload = JObject.Parse(JsonConvert.SerializeObject(new
+            {
+                Title = txtTitle.Text,
+                Message = txtMessage.Text,
+                Badge = txtBadge.Text
+            }));
 
             if (rbAndroid.Checked)
             {
-                progressBar1.Value += 5;
-                var broker = new GcmServiceBroker(new GcmConfiguration(SenderId, SenderAuthToken, null));
+                var broker = new GcmServiceBroker(new GcmConfiguration(GcmSenderId, GcmSenderAuthToken, null));
 
                 progressBar1.Value += 5;
                 broker.Start();
 
                 progressBar1.Value += 5;
-                var jsonString = Newtonsoft.Json.JsonConvert.SerializeObject(new
-                {
-                    Title = txtTitle.Text,
-                    Message = txtMessage.Text,
-                    Badge = txtBadge.Text
-                });
                 broker.QueueNotification(new GcmNotification
                 {
                     RegistrationIds = new List<string> { txtDeviceToken.Text },
-                    Data = JObject.Parse(jsonString)
+                    Data = notificationPayload
                 });
 
                 progressBar1.Value += 5;
                 broker.Stop();
+            }
+            else if (rbiOS.Checked)
+            {
+                var certificateFilePath = Path.GetDirectoryName(Application.ExecutablePath) + ApnsCertificateFile;
+                var config = new ApnsConfiguration(ApnsConfiguration.ApnsServerEnvironment.Production, certificateFilePath, ApnsCertificatePassword);
+                var broker = new ApnsServiceBroker(config);
 
                 progressBar1.Value += 5;
+                broker.Start();
+
+                progressBar1.Value += 5;
+                broker.QueueNotification(new ApnsNotification
+                {
+                    DeviceToken = txtDeviceToken.Text,
+                    Payload = notificationPayload
+                });
+
+                progressBar1.Value += 5;
+                broker.Stop();
             }
 
+            progressBar1.Value += 5;
             while (progressBar1.Value < 100)
             {
                 progressBar1.Value += 5;
