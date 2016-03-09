07fa9ab8aea75c9a8e1b20174b1e80769c2bba07
Sawan Kumar Bundelkhandi <sawankumar.bundelkhandi@cesltd.com>
2016-03-09 22:27:28 +0530

    chnahes

--- SamplePushNotificationBroadcaster/Form1.cs
+++ SamplePushNotificationBroadcaster/Form1.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using Newtonsoft.Json;
@@ -123,27 +127,34 @@ namespace SamplePushNotificationBroadcaster
             }
             else if (rbWindows.Checked)
             {
-                var config = new WnsConfiguration(WnsPackageName, WnsPackageSid, WnsClientSecret);
-                var broker = new WnsServiceBroker(config);
+                var notificationXMLString = @"<toast launch=''>
+                                                  <visual lang='en-US'>
+                                                    <binding template='ToastImageAndText01'>
+                                                      <image id='1' src='World' />
+                                                      <text id='1'>Hello</text>
+                                                    </binding>
+                                                  </visual>
+                                                </toast>";
+                //var config = new WnsConfiguration(WnsPackageName, WnsPackageSid, WnsClientSecret);
+                //var broker = new WnsServiceBroker(config);
 
-                progressBar1.Value += 5;
-                broker.Start();
+                //broker.OnNotificationSucceeded += BrokerOnOnNotificationSucceeded;
+                //broker.OnNotificationFailed += BrokerOnOnNotificationFailed;
 
-                progressBar1.Value += 5;
-                broker.QueueNotification(new WnsToastNotification
-                {
-                    ChannelUri = txtDeviceToken.Text,
-                    Payload = XElement.Parse(@"<toast>
-                            <visual>
-                                <binding template=""ToastText01"">
-                                    <text id=""1"">WNS_Send_Single</text>
-                                </binding>  
-                            </visual>
-                        </toast>")
-                });
+                //progressBar1.Value += 5;
+                //broker.Start();
 
-                progressBar1.Value += 5;
-                broker.Stop();
+                //progressBar1.Value += 5;
+                //broker.QueueNotification(new WnsToastNotification
+                //{
+                //    ChannelUri = txtDeviceToken.Text,
+                //    Payload = XElement.Parse(notificationXMLString)
+                //});
+
+                //progressBar1.Value += 5;
+                //broker.Stop();
+
+                var ret = PostToWns(WnsClientSecret, WnsPackageSid, txtDeviceToken.Text, notificationXMLString, "Toast", "text/xml");
             }
 
             progressBar1.Value += 5;
@@ -152,6 +163,17 @@ namespace SamplePushNotificationBroadcaster
                 progressBar1.Value += 5;
             }
             progressBar1.Value = 100;
+        }
+
+        private void BrokerOnOnNotificationSucceeded(WnsNotification notification)
+        {
+            Console.WriteLine(notification.ChannelUri, notification.Payload);
+        }
+
+        private void BrokerOnOnNotificationFailed(WnsNotification notification, AggregateException exception)
+        {
+            Console.WriteLine(notification.ChannelUri, notification.Payload);
+            Console.WriteLine(exception.Message);
         }
 
         private void Broker_OnNotificationSucceeded(ApnsNotification notification)
@@ -175,5 +197,132 @@ namespace SamplePushNotificationBroadcaster
             Console.WriteLine(notification.RegistrationIds?.FirstOrDefault() ?? string.Empty, notification.Notification);
             Console.WriteLine(exception.Message);
         }
+
+        // Post to WNS
+        public string PostToWns(string secret, string sid, string uri, string xml, string notificationType, string contentType)
+        {
+            try
+            {
+                // You should cache this access token.
+                var accessToken = GetAccessToken(secret, sid);
+
+                byte[] contentInBytes = Encoding.UTF8.GetBytes(xml);
+
+                HttpWebRequest request = HttpWebRequest.Create(uri) as HttpWebRequest;
+                request.Method = "POST";
+                request.Headers.Add("X-WNS-Type", notificationType);
+                request.ContentType = contentType;
+                request.Headers.Add("Authorization", String.Format("Bearer {0}", accessToken.AccessToken));
+
+                using (Stream requestStream = request.GetRequestStream())
+                    requestStream.Write(contentInBytes, 0, contentInBytes.Length);
+
+                using (HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse())
+                    return webResponse.StatusCode.ToString();
+            }
+
+            catch (WebException webException)
+            {
+                HttpStatusCode status = ((HttpWebResponse)webException.Response).StatusCode;
+
+                if (status == HttpStatusCode.Unauthorized)
+                {
+                    // The access token you presented has expired. Get a new one and then try sending
+                    // your notification again.
+
+                    // Because your cached access token expires after 24 hours, you can expect to get 
+                    // this response from WNS at least once a day.
+
+                    GetAccessToken(secret, sid);
+
+                    // We recommend that you implement a maximum retry policy.
+                    return PostToWns(uri, xml, secret, sid, notificationType, contentType);
+                }
+                else if (status == HttpStatusCode.Gone || status == HttpStatusCode.NotFound)
+                {
+                    // The channel URI is no longer valid.
+
+                    // Remove this channel from your database to prevent further attempts
+                    // to send notifications to it.
+
+                    // The next time that this user launches your app, request a new WNS channel.
+                    // Your app should detect that its channel has changed, which should trigger
+                    // the app to send the new channel URI to your app server.
+
+                    return "";
+                }
+                else if (status == HttpStatusCode.NotAcceptable)
+                {
+                    // This channel is being throttled by WNS.
+
+                    // Implement a retry strategy that exponentially reduces the amount of
+                    // notifications being sent in order to prevent being throttled again.
+
+                    // Also, consider the scenarios that are causing your notifications to be throttled. 
+                    // You will provide a richer user experience by limiting the notifications you send 
+                    // to those that add true value.
+
+                    return "";
+                }
+                else
+                {
+                    // WNS responded with a less common error. Log this error to assist in debugging.
+
+                    // You can see a full list of WNS response codes here:
+                    // http://msdn.microsoft.com/en-us/library/windows/apps/hh868245.aspx#wnsresponsecodes
+
+                    string[] debugOutput = {
+                                       status.ToString(),
+                                       webException.Response.Headers["X-WNS-Debug-Trace"],
+                                       webException.Response.Headers["X-WNS-Error-Description"],
+                                       webException.Response.Headers["X-WNS-Msg-ID"],
+                                       webException.Response.Headers["X-WNS-Status"]
+                                   };
+                    return string.Join(" | ", debugOutput);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                return "EXCEPTION: " + ex.Message;
+            }
+        }
+
+        // Authorization
+        [DataContract]
+        public class OAuthToken
+        {
+            [DataMember(Name = "access_token")]
+            public string AccessToken { get; set; }
+            [DataMember(Name = "token_type")]
+            public string TokenType { get; set; }
+        }
+
+        private OAuthToken GetOAuthTokenFromJson(string jsonString)
+        {
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(jsonString)))
+            {
+                var ser = new DataContractJsonSerializer(typeof(OAuthToken));
+                var oAuthToken = (OAuthToken)ser.ReadObject(ms);
+                return oAuthToken;
+            }
+        }
+
+        protected OAuthToken GetAccessToken(string secret, string sid)
+        {
+            var urlEncodedSecret = WebUtility.UrlEncode(secret);
+            var urlEncodedSid = WebUtility.UrlEncode(sid);
+
+            var body = $"grant_type=client_credentials&client_id={urlEncodedSid}&client_secret={urlEncodedSecret}&scope=notify.windows.com";
+
+            string response;
+            using (var client = new WebClient())
+            {
+                client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+                response = client.UploadString("https://login.live.com/accesstoken.srf", body);
+            }
+            return GetOAuthTokenFromJson(response);
+        }
+
     }
 }
